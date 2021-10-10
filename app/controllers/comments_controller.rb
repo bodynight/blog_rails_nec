b03d5d299f1cce982e40832c4660fcf955be531b
bodynight <xbodynightx@gmail.com>
2021-10-10 19:41:22 +0300

    cheng views and input comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,17 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+    coment = @article.comments.new 
+    coment.author = current_user.username
+    coment.body = comment_params[:body]
+    coment.save
+
     redirect_to article_path(@article)
   end
 private
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit( :body)
+
   end
 
 end
