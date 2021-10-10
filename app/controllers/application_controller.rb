cc865213952d681104ecc70fd22442bfda52ad1b
bodynight <xbodynightx@gmail.com>
2021-10-10 17:16:51 +0300

    fix for new version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
 
   private
   
-def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) << :username
+def configure_permitted_parametrs
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
    # devise_parameter_sanitizer.for(:sign_in) << :username
    # devise_parameter_sanitizer.for(:account_update)  << :username
 end 
