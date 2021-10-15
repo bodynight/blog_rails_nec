679602ed050140ae740bbcbae5b8e3f851656689
bodynight <xbodynightx@gmail.com>
2021-10-15 22:44:15 +0300

    add articlename in article controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.articlename = current_user.username
     if @article.save
       redirect_to @article
     else
