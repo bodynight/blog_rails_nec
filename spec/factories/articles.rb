74ae9d342b04c37f187f2b314e464702e7b4bb76
bodynight <xbodynightx@gmail.com>
2021-10-18 23:10:58 +0300

    update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,17 @@
 FactoryGirl.define do
   factory :article do
-    title  "Lorem Ipsum"
+    title  "Article title"
     text "article text"
-    
-   end
+
+    #создаем фабрику с названием article_with_comments
+    #для создания статьи с несколькими коментариями
+    factory :article_with_comments do
+      #после создания article
+      after :create do |article, evaluator|
+        #cоздаем список из 3-х коментариев
+        create_list :comment, 3, article: article
+      end
+    end
+
   end
+end
